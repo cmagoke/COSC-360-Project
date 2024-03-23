9b8eedbba1456dffe531c7e9b1fe45d07c3cbeb5
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-03-23 12:05:57 -0700

    Update processLogin.php

--- processLogin.php
+++ processLogin.php
@@ -7,15 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['user'];
     $password = $_POST['pass'];
     if(isset($username) && isset($password)){
+        $passwordHash = md5($password);
         $sql = "SELECT * FROM User WHERE Username = ? AND Password = ?";
         $stmt = $con->prepare($sql);
-        $stmt -> bind_param("ss", $username, $password);
+        $stmt -> bind_param("ss", $username, $passwordHash);
         $stmt -> execute();
         $result = $stmt->get_result();
         $row = $result->fetch_assoc();
         if(!is_null($row)){
             $_SESSION['user'] = $row["UserId"];
-            header("Location: homepage.php");
+            if($row["UserId"] == 1){
+                header("Location: adminpage.php");
+            }else{
+                header("Location: homepage.php");
+            }
         }else{
             echo "Incorrect user/password";
         }
