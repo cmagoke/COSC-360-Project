827a0da14a287b6d3715074c58534ff05ecde3e1
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-03-22 23:28:07 -0700

    Update editPost.php

--- editPost.php
+++ editPost.php
@@ -1,3 +1,7 @@
+<?php
+  include "db.php";
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,15 +11,18 @@
     <link rel="stylesheet" href="./css/poststyles.css" />
   </head>
   <body>
+    <?php
+        if(!isset($_SESSION['user']) || $_SESSION['user'] != 1 ){
+            header("Location: signin.php");
+        }          
+    ?>
     <div class="header">
     <a href="adminpage.php">TextHub Admin</a>
-    <button id="logout" onclick="window.location.href='homepage.php'">Log Out</button>
+    <button id="logout" onclick="window.location.href='processLogout.php'">Log Out</button>
     </div>
     <div class="rectangle">
     <form action="processEdit.php" method="post">
         <?php
-        include "db.php";
-
         if($_SERVER["REQUEST_METHOD"] == "GET"){
             if(isset($_GET['p'])){
                 $postid = $_GET['p'];
