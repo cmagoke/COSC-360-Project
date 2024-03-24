1474dc93feaa8cf9c46b459105dc67a4ac658adf
cmagoke <113209594+cmagoke@users.noreply.github.com>
2024-03-23 21:31:58 -0700

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,12 +1,12 @@
 <?php
 //local
-define('dbhost','localhost');
+/*define('dbhost','localhost');
 define('dbname','root');
 define('dbuser','');
-define('dbpass','cosc 360');
+define('dbpass','cosc 360');*/
 //server
-/* define('dbhost','localhost');
+define('dbhost','localhost');
 define('dbname','17700014');
 define('dbuser','17700014');
-define('dbpass','db_17700014'); */
+define('dbpass','db_17700014'); 
 ?>
