d878950c688071d3cef02c1e621684283f904564
alericg <112976583+alericg@users.noreply.github.com>
2024-03-23 19:49:59 -0700

    Add files via upload

--- subforum_page.php
+++ subforum_page.php
@@ -1,29 +1,70 @@
 <?php
  include "db.php";
  session_start();
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-   <title>TextHub</title>
-   <link rel="stylesheet" href="css/styles.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TextHub Homepage</title>
+    <link rel="stylesheet" href="./css/styles.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/buttons.js"></script>
 </head>
 <body>
     <div id="header">
-        <a href="homepage.html" id="top-logo">TextHub</a>
-        <input type="search" placeholder="Search">
-        <a href="signin.html" class="sign"><button id="login" type="button">Log in</button></a>
-        <a href="signup.html" class="sign"><button id="signup" type="button">Sign Up</button></a>
+        <a href="homepage.php">TextHub</a>
+        <form action="search.php" method="get">
+            <input type="search" id="search" name="search" placeholder="Search">
+            <button type="submit">Search</button>
+        </form>
+        <?php
+             if(isset($_SESSION['user'])){
+                $userid = $_SESSION['user'];
+                $sql = "SELECT Username FROM User WHERE UserId = ?";
+                $stmt = $con->prepare($sql);
+                $stmt->bind_param("i", $userid);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                if(!is_null($row)){
+                    echo "<div><h2> Hello ". $row['Username'] . "!</h2></div>
+                    <button id=\"logout\" onclick=\"window.location.href='processLogout.php'\">Log Out</button>";
+                }else{
+                    echo "failed to get user";
+                }
+             }else{
+                echo "<button id=\"login\" onclick=\"window.location.href='signin.php'\">Log in</button>
+                <button id=\"signup\" onclick=\"window.location.href='signup.php'\">Sign Up</button>";
+             }
+        ?>
     </div>
+    
     <div id="forumhead">
         <div class="profile-pic">
             <img src="images/calc.jpeg" alt="Profile Picture">
           </div>
+
           <div class="subreddit-info">
-            <h1 class="subreddit-title">Calculus</h1>
-            <span class="subreddit-name">r/calculus</span>
+          <?php
+            if(isset($_GET['name'])){
+                $forumName = $_GET['name'];
+                $sql = "SELECT * FROM subforum WHERE Name = ?;";
+                $stmt = $con->prepare($sql);
+                $stmt->bind_param("i", $userid);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                if(!is_null($row)){
+                    echo "<h1 class='subreddit-title'>".$row['Name']."</h1>";
+                    echo "<span class='subreddit-name'>t/".$row['Name']."</span>";
+                }
+            }
+          ?>
           </div>
           <button class="join-button">Join</button>
         </header>
@@ -34,56 +75,60 @@
             <li><a href="#">Resources</a></li>
           </ul>
         </nav>
+        <button id="AddPostButton"><a href="posting.php">Add Post</a></button>
     </div>
     <div id="mainbody">
         <div id="about">
             <div class="subtitle">About Community</div>
+            
             <ul>
-                <li>We are a community of individuals who have a love and respect of calculus or wish to learn more.</li>
-                <li>425k Mathmaticians</li>
-                <li>Created 14th March 2016</li>
+            <?php
+            if(isset($_GET['name'])){
+                $forumName = $_GET['name'];
+                $sql = "SELECT * FROM subforum WHERE Name = ?;";
+                $stmt = $con->prepare($sql);
+                $stmt->bind_param("i", $forumName);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                if(!is_null($row)){
+                    echo "<li>".$row['About']."</li>";
+                    echo "<li>".$row['NumOfUsers']." Members</li>";
+                    echo "<li>Created ".$row['Created']."</li>";
+                }
+            }
+          ?>
             </ul>
         </div>
         <div id="main">
-            <div class="entry">
-                <div class="heading">t/calculus - 3 hours ago</div>
-                <div class="username">johndoe</div>
-                <div class="title">How to do intregrals!!?!?!?!</div>
-                <p>I am stuck on a intregral problem and don’t know what is the right way to approach it. I have posted a screen shot of the problem below. Any help will be appreciated !!!!</p>
-                <div class="vote_comment">
-                    <div><img src="images/up_arrow.jpg"></div>
-                    <div>20</div>
-                    <div><img src="images/down_arrow.jpg"></div>
-                    <div><img src="images/comment_icon.png"></div>
-                    <div>30</div>
-                </div>
-            </div>
-            <div class="entry">
-                <div class="heading">t/calculus - 2 days ago</div>
-                <div class="username">MathMan123</div>
-                <div class="title">Just aced my Calc 4 test</div>
-                <p>I'm finally done with Calculus!!!!</p>
-                <div class="vote_comment">
-                    <div><img src="images/up_arrow.jpg"></div>
-                    <div>-5</div>
-                    <div><img src="images/down_arrow.jpg"></div>
-                    <div><img src="images/comment_icon.png"></div>
-                    <div>87</div>
-                </div>
-            </div>
-            <div class="entry">
-                <div class="heading">t/calculus - 5 days ago</div>
-                <div class="username">piGuy314</div>
-                <div class="title">OPINION: Newton or Leibniz is the founder of Calculus?</div>
-                <p>It is known that calculus is nothing without these two scientists/mathematicians, however, what is your opinion on who the true founder is? Leibniz is known for coining the name, while Newton laid the foundation?</p>
-                <div class="vote_comment">
-                    <div><img src="images/up_arrow.jpg"></div>
-                    <div>256</div>
-                    <div><img src="images/down_arrow.jpg"></div>
-                    <div><img src="images/comment_icon.png"></div>
-                    <div>16</div>
-                </div>
-            </div>
+                <?php
+                if(isset($_GET['name'])){
+                    $forumName = $_GET['name'];
+                    $sql = "SELECT * FROM Post WHERE Subforum = ?";
+                    $stmt = $con->prepare($sql);
+                    $stmt->bind_param("i", $forumName);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $row = $result->fetch_assoc();
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo "
+                        <div class=\"entry\">
+                        <div class=\"heading\" >" . $row['Subforum'] . " - " . $row['DateTime'] . "</div>
+                        <div class=\"username\">" . $row['Username'] . "</div>
+                        <div class=\"title\" onclick=\"window.location.href='single_post.php'\">" . $row['Title'] . "</div>
+                        <p>" . $row['Description'] ."</p>
+                        <div class=\"vote_comment\">
+                            <div><img class=\"up_arrow\" src=\"images/up_arrow.jpg\" onclick=\"upvote(". $row['PostId'] . ")\"></div>
+                            <div class=\"vote_count\">" . $row['VotesNum'] . "</div>
+                            <div><img class=\"down_arrow\" src=\"images/down_arrow.jpg\"  onclick=\"downvote(". $row['PostId'] . ")\"></div>
+                            <div><img class=\"comment-icon\" src=\"images/comment_icon.png\"></div>
+                            <div class=\"comment_count\">". $row['CommentsNum'] . "</div>
+                        </div>
+                        </div>";
+                    }
+                    
+                }
+                ?>
         </div>
     </div>  
     <footer>
